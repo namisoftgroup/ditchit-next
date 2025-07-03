959080c7b5b0156698a0c4f0d0f8986b1cadbbbf
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-07-03 16:16:03 +0300

    feat: implement PageBanner component with breadcrumb navigation; update PostDetails to use PageBanner

--- src/app/(main)/all-posts/[productId]/page.tsx
+++ src/app/(main)/all-posts/[productId]/page.tsx
@@ -1,7 +1,15 @@
+import PageBanner from "@/components/shared/PageBanner";
+
 export default function PostDetails() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <PageBanner
+        links={[
+          { title: "Home", link: "/" },
+          { title: "All Posts", link: "/all-posts" },
+        ]}
+        page="Post Details"
+      />
+    </>
+  );
 }
---
+++ src/components/shared/PageBanner.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "../ui/breadcrumb";
+import { Fragment } from "react";
+
+type BreadcrumbLinkType = {
+  title: string;
+  link: string;
+};
+
+export default function PageBanner({
+  links,
+  page,
+}: {
+  links: BreadcrumbLinkType[];
+  page: string;
+}) {
+  return (
+    <div className="bg-[#f5f5f5] py-5 relative isolate">
+      <div className="mx-auto max-w-[1440px] px-2">
+        <Breadcrumb>
+          <BreadcrumbList>
+            {links.map((item, index) => (
+              <Fragment key={index}>
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    <Link href={item.link} className="capitalize">
+                      {item.title}
+                    </Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+              </Fragment>
+            ))}
+
+            <BreadcrumbItem>
+              <BreadcrumbPage className="text-[var(--grayColor)]">
+                {page}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    </div>
+  );
+}
