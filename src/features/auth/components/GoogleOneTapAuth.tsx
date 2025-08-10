1b620997ce081e20c655189edba9914af4480292
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-08-10 15:42:32 +0300

    feat(auth): implement google one tap login with user authentication
    - Add user authentication flow after successful Google One Tap login
    - Store user data and token in auth store upon successful login
    - Redirect to home page and show success toast on login
    - Handle errors with appropriate error toasts

--- src/features/auth/components/GoogleOneTapAuth.tsx
+++ src/features/auth/components/GoogleOneTapAuth.tsx
@@ -1,13 +1,52 @@
 "use client";
 
 import { useGoogleOneTapLogin } from "@react-oauth/google";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "../store";
+import { toast } from "sonner";
+import { authAction } from "../actions";
+
+import axios from "axios";
 
 export default function GoogleOneTapAuth() {
+  const router = useRouter();
+  const { setUser, setToken } = useAuthStore((state) => state);
+
   useGoogleOneTapLogin({
-    onSuccess: (credentialResponse) => {
-      console.log(credentialResponse);
+    onSuccess: async (tokenResponse) => {
+      const res = await axios.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        {
+          headers: {
+            Authorization: `Bearer ${tokenResponse.credential}`,
+          },
+        }
+      );
+
+      const formData = new FormData();
+
+      formData.append("email", res.data.email);
+      formData.append("name", res.data.name);
+      formData.append("social_type", "google");
+      formData.append("social_id", res.data.sub);
+
+      try {
+        const res = await authAction(formData, "/auth/socialLogin");
+
+        if (res.code === 200) {
+          setUser(res.data.user);
+          setToken(res.data.auth.token);
+          router.push("/");
+          toast.success("Login successful");
+        } else {
+          toast.error(res.message);
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("Something went wrong");
+      }
     },
-    
+
     onError: () => {
       console.log("Google One Tap failed");
     },
