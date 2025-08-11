4cf33033589a46cd881f705f833c0adbeeca72a5
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-08-11 11:43:49 +0300

    fix(queries): enable queries only when authenticated
    Add token check from auth store to enable queries only when user is authenticated. This prevents unnecessary API calls when user is not logged in.

--- src/hooks/queries/useGetFavorites.ts
+++ src/hooks/queries/useGetFavorites.ts
@@ -1,12 +1,16 @@
 "use client";
 
+import { useAuthStore } from "@/features/auth/store";
 import { getMyFavorites } from "@/features/profile/service";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useGetMyFavorites() {
+  const { token } = useAuthStore();
+  
   const { isLoading, data, error, refetch } = useQuery({
     queryKey: ["my-favorites"],
     queryFn: () => getMyFavorites(),
+    enabled: Boolean(token),
   });
   return { isLoading, data, error, refetch };
 }
--- src/hooks/queries/useGetMyPosts.ts
+++ src/hooks/queries/useGetMyPosts.ts
@@ -2,11 +2,15 @@
 
 import { getMyPosts } from "@/features/profile/service";
 import { useQuery } from "@tanstack/react-query";
+import { useAuthStore } from "@/features/auth/store";
 
 export default function useGetMyPosts() {
+  const { token } = useAuthStore();
+
   const { isLoading, data, error, refetch } = useQuery({
     queryKey: ["my-posts"],
     queryFn: () => getMyPosts(),
+    enabled: Boolean(token),
   });
   return { isLoading, data, error, refetch };
 }
